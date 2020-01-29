dd44c391d6614079900f5c54de22556fbbe7953a
Vikas Dhiman <wecacuee@users.noreply.github.com>
2020-01-29 00:19:25 -0800

    also decide on a dataset directory

--- setup.bash
+++ setup.bash
@@ -5,3 +5,4 @@ source $DIR/devel/setup.bash
 export PYTHONPATH=$PYTHONPATH:/opt/ros/melodic/lib/python2.7/dist-packages
 #export PYTHONPATH=/usr/local/lib/python3.6/dist-packages:$PYTHONPATH
 export ROS_PYTHON_VERSION=3
+export DATA_DIR=$HOME/dataset
