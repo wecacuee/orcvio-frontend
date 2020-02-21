d693aeb50d459de66b8e8ceb3822a17f81cd6fc3
Vikas Dhiman <wecacuee@users.noreply.github.com>
2020-02-20 17:07:34 -0800

    updated bonnetal

--- install-deps/install-libtorch.sh
+++ install-deps/install-libtorch.sh
@@ -1,4 +1,4 @@
 wget https://download.pytorch.org/libtorch/cu101/libtorch-cxx11-abi-shared-with-deps-1.4.0.zip -O /tmp/libtorch-cxx11-abi-shared-with-deps-1.4.0.zip
-mkdir -p $CATKIN_WORKSPACE/devel/ && \
-unzip /tmp/libtorch-cxx11-abi-shared-with-deps-1.4.0.zip -d $CATKIN_WORKSPACE/devel/
-rsync -arP $CATKIN_WORKSPACE/devel/libtorch/ $CATKIN_WORKSPACE/devel/
+mkdir -p $CATKIN_WORKSPACE/devel/stow/ && \
+unzip /tmp/libtorch-cxx11-abi-shared-with-deps-1.4.0.zip -d $CATKIN_WORKSPACE/devel/stow/
+cd $CATKIN_WORKSPACE/devel/stow && stow libtorch
