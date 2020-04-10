1e19c62405ef17ff516398682395eb891f13cd57
Vikas Dhiman <wecacuee@users.noreply.github.com>
2020-04-10 10:04:34 -0700

    clone only if file not present

--- install-deps/install-gsl.sh
+++ install-deps/install-gsl.sh
@@ -3,7 +3,8 @@
 {
     SOURCE_PREFIX=$HOME/.local/src
     STOW_PREFIX=$SOURCE_PREFIX/stow
-git clone --branch v0.36.0 https://github.com/gsl-lite/gsl-lite.git $SOURCE_PREFIX/gsl-lite  && \
+    { [ -f $SOURCE_PREFIX/gsl-lite/CMakeLists.txt ] || \
+        git clone --branch v0.36.0 https://github.com/gsl-lite/gsl-lite.git $SOURCE_PREFIX/gsl-lite; } && \
     cd $SOURCE_PREFIX/gsl-lite && \
     cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=$STOW_PREFIX/gsl-0.36.0 && \
     cmake --build build --target install && \
