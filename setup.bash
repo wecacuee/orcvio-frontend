571b6e39e64912b8f0531210e61e5f552036cf4d
Vikas Dhiman <wecacuee@users.noreply.github.com>
2020-05-07 16:05:25 -0700

    starmap update

--- setup.bash
+++ setup.bash
@@ -1,11 +1,11 @@
 DIR=$(dirname $(readlink -m ${BASH_SOURCE[0]}))
 #source /opt/ros/melodic/setup.bash
-source $DIR/devel/setup.bash
 export CMAKE_PREFIX_PATH=${HOME}/aux/orcvio-frontend/devel:$CMAKE_PREFIX_PATH
 
+export ROS_PYTHON_VERSION=3
 export PYTHONPATH=$PYTHONPATH:/opt/ros/melodic/lib/python2.7/dist-packages
 #export PYTHONPATH=/usr/lib/python3/dist-packages/:$PYTHONPATH
 #export PYTHONPATH=/usr/local/lib/python3.6/dist-packages:$PYTHONPATH
-export ROS_PYTHON_VERSION=3
 export DATA_DIR=$HOME/dataset
 source ${DIR}/.tox/py3?/bin/activate
+[ -f $DIR/devel/setup.bash ] && source $DIR/devel/setup.bash
