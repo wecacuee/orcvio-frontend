93968b4d71d6e9e0382f910f107e301beec7fbb4
Vikas Dhiman <wecacuee@users.noreply.github.com>
2020-05-28 14:09:31 -0700

    update the child repositories

--- install-deps/clone-deps-and-catkin-build.sh
+++ install-deps/clone-deps-and-catkin-build.sh
@@ -1,10 +1,11 @@
 
 # Install vision_opencv and open_vins
 {
-mkdir -p $CATKIN_WORKSPACE/src && cd $CATKIN_WORKSPACE && \
-    git clone https://github.com/ros-perception/vision_opencv.git src/vision_opencv && \
-    git clone https://github.com/rpng/open_vins/ src/open_vins && \
+mkdir -p $CATKIN_WORKSPACE/src; cd $CATKIN_WORKSPACE && \
     . /opt/ros/melodic/setup.sh && \
     catkin config -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so && \
+    cd src && wstool init ; \
+        rosinstall_generator image_pipeline rosbag vision_opencv | wstool merge - && \
+    cd -
     catkin build
 }
--- setup.bash
+++ setup.bash
@@ -1,6 +1,7 @@
 DIR=$(dirname $(readlink -m ${BASH_SOURCE[0]}))
 #source /opt/ros/melodic/setup.bash
 export CMAKE_PREFIX_PATH=${HOME}/aux/orcvio-frontend/devel:$CMAKE_PREFIX_PATH
+export PYTHONPATH=${HOME}/aux/orcvio-frontend/devel/lib/python3.6/dist-packages:$PYTHONPATH
 
 export ROS_PYTHON_VERSION=3
 export PYTHONPATH=$PYTHONPATH:/opt/ros/melodic/lib/python2.7/dist-packages
